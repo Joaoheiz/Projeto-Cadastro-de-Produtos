1a89df386802a0a18267d32d50348b8bda013e42
João Pedro <joaoheiz1999@gmail.com>
2023-03-14 12:52:02 -0300

    teste commit

---
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,23 @@
+import { HeaderService } from './../../components/template/header/header.service';
+ import { Component } from '@angular/core';
+ import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
+})
+export class ProductCrudComponent {
+
+  constructor(private router: Router, private headerService: HeaderService) {
+    headerService.headerData = {
+      title: 'Cadastro de Produtos', 
+      icon: 'storefront', 
+      routeURL: '/products'
+    }
+  }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
+  }
+}
